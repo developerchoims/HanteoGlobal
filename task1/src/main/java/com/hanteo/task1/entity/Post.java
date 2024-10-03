ae942229ab82c8353a1de7557f4e91c7e8d7606e
developerchoims <developerchoims@gmail.com>
2024-10-03 15:56:26 +0900

    10/03 - entity 구조 수정

--- task1/src/main/java/com/hanteo/task1/entity/Post.java
+++ task1/src/main/java/com/hanteo/task1/entity/Post.java
@@ -1,6 +1,8 @@
 package com.hanteo.task1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
@@ -17,17 +19,23 @@ public class Post {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
+    @Size(max = 255)
+    @NotNull
     @Column(name = "title", nullable = false)
     private String title;
 
+    @NotNull
     @Lob
     @Column(name = "content", nullable = false)
     private String content;
 
+    @Size(max = 255)
+    @NotNull
     @Column(name = "author", nullable = false)
     private String author;
 
--- task1/src/main/java/com/hanteo/task1/service/CategoryService.java
+++ task1/src/main/java/com/hanteo/task1/service/CategoryService.java
@@ -1,9 +1,6 @@
 package com.hanteo.task1.service;
 
-import com.hanteo.task1.entity.Category;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 
 public interface CategoryService {
     ResponseEntity<?> getCategoryByCategoryId(Integer categoryId);
